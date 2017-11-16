f50a6cf4c765d0ad9e630ca6cf65afe170cfcb78
aokiyuya <yuya.aoki@fujielab.org>
2017-11-17 00:49:23 +0900

    音声認識で画像を変えられるようにした

--- GUI_for_team_B.py
+++ GUI_for_team_B.py
@@ -1,13 +1,12 @@
-#! !(which python)
-# coding: utf-8
-###########################
+#! !(which python) # coding: utf-8
+############################
 # Author: Yuya Aoki
 #
 ###########################
 from logging import getLogger, basicConfig, DEBUG, StreamHandler
-from threading import Thread
-from datetime import datetime as dt
-import os
+# from threading import Thread
+# from datetime import datetime as dt
+# import os
 import re
 import threading
 import datetime
@@ -15,21 +14,19 @@ import socket
 import atexit
 import sys
 import subprocess
-import time
-import xml.sax
-import xml.sax.handler
+# import time
 from kivy.app import App
 from kivy.uix.button import Button, Label
 from kivy.uix.image import Image
 from kivy.config import Config
 from kivy.uix.actionbar import ActionBar, ActionButton
 from kivy.uix.actionbar import ActionView, ActionPrevious
-from kivy.uix.textinput import TextInput
+# from kivy.uix.textinput import TextInput
 from kivy.core.text import LabelBase, DEFAULT_FONT
 from kivy.resources import resource_add_path
 from kivy.uix.floatlayout import FloatLayout
-from kivy.core.image import Image as CoreImage
-from kivy.uix.scatter import Scatter
+# from kivy.core.image import Image as CoreImage
+# from kivy.uix.scatter import Scatter
 
 # 日本語扱う設定
 resource_add_path('~/')
@@ -56,41 +53,44 @@ SAY_SPEED = 100
 
 THRESHOLD = 1
 
+
 # open jtalk を立ち上げておく
-cmd = ['/usr/local/open_jtalk-1.08-STRAIGHT-3.0.0/bin/run_open_jtalk.sh',
-       '--',
-       '-i',
-       'iwata_kana',
-       '-op',
-       '-p',
-       str(SAY_SPEED)]
-p = subprocess.Popen(cmd, stdin=subprocess.PIPE)
+class Openjtalk_straight(object):
+    def __init__(self):
+        self.start_openjtalk()
 
-cmd = ['/usr/local/open_jtalk-1.08-STRAIGHT-3.0.0/bin/run_open_jtalk.sh',
-       '--',
-       '-op',
-       '-p',
-       str(SAY_SPEED)]
-speak_stdin_proc = subprocess.Popen(cmd, stdin=subprocess.PIPE)
+    def start_openjtalk(self):
+        cmd = ['/usr/local/open_jtalk-1.08-straight-3.0.0/bin/'
+               + 'run_open_jtalk.sh',
+               '--',
+               '-i',
+               'iwata_kana',
+               '-op',
+               '-p',
+               str(SAY_SPEED)]
+        self.p = subprocess.Popen(cmd, stdin=subprocess.PIPE)
 
-def change_speed_jtalk(speed):
-    global p, SAY_SPEED, speak_stdin_proc
-    p.terminate()
-    SAY_SPEED = SAY_SPEED + speed
-    cmd = ['/usr/local/open_jtalk-1.08-straight-3.0.0/bin/run_open_jtalk.sh',
-           '--',
-           '-i',
-           'iwata_kana',
-           '-op',
-           '-p',
-           str(SAY_SPEED)]
-    p = subprocess.Popen(cmd, stdin=subprocess.PIPE)
-    cmd = ['/usr/local/open_jtalk-1.08-STRAIGHT-3.0.0/bin/run_open_jtalk.sh',
-           '--',
-           '-op',
-           '-p',
-           str(SAY_SPEED)]
-    speak_stdin_proc = subprocess.Popen(cmd, stdin=subprocess.PIPE)
+    def terminate(self):
+        self.p.terminate()
+
+    def change_speed_jtalk(self, speed):
+        global SAY_SPEED
+        self.p.terminate()
+        SAY_SPEED = SAY_SPEED + speed
+        logger.info(SAY_SPEED)
+        cmd = ['/usr/local/open_jtalk-1.08-straight-3.0.0/bin/'
+               + 'run_open_jtalk.sh',
+               '--',
+               '-i',
+               'iwata_kana',
+               '-op',
+               '-p',
+               str(SAY_SPEED)]
+        self.p = subprocess.Popen(cmd, stdin=subprocess.PIPE)
+
+    def talk(self, string):
+        self.p.stdin.write(string.encode('utf-8'))
+        self.p.stdin.flush()
 
 
 # ログを保存
@@ -104,8 +104,7 @@ def write_dialogue(string):
 def at_exit():
     # print('\nExit')
     logger.info('exit')
-    p.terminate()
-    speak_stdin_proc.terminate()
+    openjtalk.terminate()
     sys.exit(0)
 
 
@@ -123,17 +122,7 @@ def speak_with_iwata_kana(string):
     input_string = string
     if '\n' not in string:
         input_string = string + '\n'
-    p.stdin.write(input_string.encode('utf-8'))
-    p.stdin.flush()
-
-
-# 岩田カナ文でない文章を音声合成
-def speak_jtalk(string):
-    input_string = string
-    if '\n' not in string:
-        input_string = string + '\n'
-    speak_stdin_proc.stdin.write(input_string.encode('utf-8'))
-    speak_stdin_proc.stdin.flush()
+    openjtalk.talk(input_string)
 
 
 # 別スレッドで発話
@@ -154,6 +143,7 @@ def say_command(arg_string):
 def command():
     output('command')
 
+
 # kivyで日本語入力がうまくいかないので急遽作成
 def speak_read():
     read_lines = ''
@@ -166,25 +156,14 @@ def speak_read():
             continue
         elif 'exit' in read_line:
             exit(0)
-        elif 'change' in read_line:
-            change_speed_jtalk(int(read_line.split(' ')[1]))
         read_lines += read_line
         if ';' in read_line:
             write_dialogue('say ' + read_lines.replace(';', '') + '\n')
-            speak_jtalk(read_lines.replace(';', '') + '\n')
+            say_command(read_lines.replace(';', '') + '\n')
             output('said')
             read_lines = ''
         # print(read_lines, end='')
 
-
-class Picture(Scatter):
-    source = './al.png'
-    size_hint = .4, .4
-    Image = {
-        'id': 'image',
-        'source': source
-    }
-    # pos_hint = (-36, -36)
 
 class MyActionBar():
     def __init__(self):
@@ -224,20 +203,33 @@ class RootWidget(FloatLayout):
     def __init__(self, **kwargs):
         super(RootWidget, self).__init__(**kwargs)
         # self.cols = 3
-        self.picture = Image(
+        global picture_list
+        picture_list.append(Image(
             source='./al.png',
             size_hint=(.4, .4),
             pos_hint={'center_x': .5, 'center_y': .6}
-        )
+        ))
+        picture_list.append(Image(
+            source='./TOFU_notlogo.png',
+            size_hint=(.4, .4),
+            pos_hint={'center_x': .5, 'center_y': .6}
+        ))
+        picture_list.append(Image(
+            source='./TOFU_notlogo_2.png',
+            size_hint=(.4, .4),
+            pos_hint={'center_x': .5, 'center_y': .6}
+        ))
+        self.picture = picture_list[0]
+        threading.Thread(target=self.make_server).start()
         self.build()
 
     def build(self):
-        global output_label
-        self.picture.reload()
+        global output_label, picture_list
+        # self.picture.reload()
         self.clear_widgets()
         self.add_widget(self.picture)
         self.A = Button(
-            text='具材A',
+            text='具材1',
             size_hint=(.2, .08),
             font_size=35,
             pos_hint={'center_x': .3, 'center_y': .3}
@@ -246,7 +238,7 @@ class RootWidget(FloatLayout):
         self.add_widget(self.A)
 
         self.B = Button(
-            text='具材B',
+            text='具材2',
             size_hint=(.2, .08),
             font_size=35,
             pos_hint={'center_x': .5, 'center_y': .3}
@@ -255,7 +247,7 @@ class RootWidget(FloatLayout):
         self.add_widget(self.B)
 
         self.C = Button(
-            text='具材C',
+            text='具材3',
             font_size=35,
             size_hint=(.2, .08),
             pos_hint={'center_x': .7, 'center_y': .3}
@@ -266,36 +258,64 @@ class RootWidget(FloatLayout):
         # change motor angle
 
         output_label = Label(
-                text='no out put',
-                size_hint=(.3, .08),
-                font_size=25,
-                pos_hint={'center_x': .3, 'center_y': .1}
-            )
+            text='no out put',
+            size_hint=(.3, .08),
+            font_size=25,
+            pos_hint={'center_x': .3, 'center_y': .1}
+        )
         self.add_widget(output_label)
 
         # self.action_bar = MyActionBar()
 
         # self.add_widget(self.action_bar.actionbar)
 
-
     def on_click_A(self, instance):
-        self.picture = Image(
-            source='./TOFU_notlogo.png',
-            size_hint=(.4, .4),
-            pos_hint={'center_x': .5, 'center_y': .6}
-        )
-        self.build()
+        self.picture_reload(0)
+        output('click')
 
     def on_click_B(self, instance):
-        self.picture = Image(
-            source='./TOFU_notlogo_2.png',
-            size_hint=(.4, .4),
-            pos_hint={'center_x': .5, 'center_y': .6}
-        )
-        self.build()
+        self.picture_reload(1)
+        output('click')
 
     def on_click_C(self, instance):
-        pass
+        self.picture_reload(2)
+        output('click')
+
+    def make_server(self, host='', port=39400):
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sp:
+            sp.bind((host, port))
+            sp.listen(2)
+            while server_is_running:
+                data_str = ''
+                client_socket, address = sp.accept()
+                while True:
+                    # 深い意味はない
+                    data = client_socket.recv(1024)
+                    if not data:
+                        break
+                    data_str = data_str + data.decode('utf-8')
+                print(data_str)
+                client_socket.close()
+                if re.match(r'[あ-ん一-龥0-9]', data_str):
+                    global user_state
+                    user_state = 'finish'
+                    if re.match(r'[1一]', data_str):
+                        self.picture_reload(0)
+                    if re.match(r'[2二]', data_str):
+                        self.picture_reload(1)
+                    if re.match(r'[3三]', data_str):
+                        self.picture_reload(2)
+                else:
+                    pass
+
+    def picture_reload(self, num):
+        global picture_list
+        # self.picture = Image(
+        self.picture = picture_list[num]
+        # picture_list.source = source_file
+        # self.picture.reload()
+        self.build()
+
 
 class TestApp(App):
     def __init__(self):
@@ -308,173 +328,6 @@ class TestApp(App):
         return self.root
 
 
-# ストリームをラッピングするクラス
-class StreamWrapperForJulius(object):
-    u"""Juliusの結果をSAXパーサが振る舞えるようにするためのラッパー"""
-
-    def __init__(self, stream):
-        u"""コンストラクタ"""
-        # ストリーム
-        self._stream = stream
-        # ヘッダが読み込まれたかどうか
-        self._header_is_read = 0
-
-    def read(self, size):
-        u"""ストリームから読み込みを行う．SAXパーサはこのメソッドだけ呼ぶ"""
-
-
-        # ヘッダが読まれてなければヘッダを返す
-        if self._header_is_read < 2:
-            self._header_is_read += 1
-            return '<ROOT>\n'
-
-        # 要求されたサイズは無視して，１行分読み込む
-        result = self._stream.readline()
-        # '.' だけが入ってくる場合があるので，その場合はスキップする．
-        while result == '.\n':
-            result = self._stream.readline()
-        # 空白はいらない．また，<s>と</s>は特別なキャラクタ'<', '>'を
-        # 含んでいるので変換する
-        result = result.strip()
-        result = result.replace('<s>', '&lt;s&gt;')
-        result = result.replace('</s>', '&lt;/s&gt;')
-        return result
-
-    def __getattr__(self, attr):
-        # その他のメソッドを本物のストリームのメソッドにする
-        return getattr(self._stream, attr)
-
-
-# 終了要求受理を意味する例外クラス
-class TerminateRequestedException(Exception):
-    def __init__(self, value=None):
-        self.value = value
-
-    def __str__(self):
-        return repr(self.value)
-
-
-# XMLハンドラ
-class JuliusResultHandler(xml.sax.handler.ContentHandler):
-    u"""Juliusの結果のハンドラ.
-
-    コンストラクタに引数が与えられた場合，ParserThreadの
-    オブジェクトであることを前提とする．
-    _runningプロパティがFalseの際にstartElementメソッドは
-    （正常終了を表す）TerminateRequestedExceptionを投げる．
-    """
-
-    def __init__(self, thread=None):
-        self._thread = thread
-        self.dialog_manager = Dialog_manager()
-        self.startTime = dt.today()
-        self.stopTime = dt.today()
-        self.diff = dt.today()
-
-    # 時間インスタンスをreturn
-    def setStartTime(self):
-        self.startTime = dt.today()
-        logger.info('setStartTime')
-
-    def setStopTime(self):
-        global user_state
-        self.stopTime = dt.today()
-        logger.info('setStopTime')
-        self.diff = self.stopTime - self.startTime
-        logger.info('total_seconds:' + str(self.diff.total_seconds()))
-        if self.diff.total_seconds() > THRESHOLD:
-            user_state = 'finish'
-
-        # 時間を文字列で出力
-    def getTimeString(self, time):
-        d = time
-        logger.debug(d.strftime('%Y/%m/%d %H:%M:%S.%f'))
-        return d.strftime('%Y/%m/%d %H:%M:%S.%f')
-
-
-    def startElement(self, name, attrs):
-            # 終了要求されている場合はTerminateRequestedExceptionを投げる
-        if self._thread is not None and self._thread._running is False:
-            raise TerminateRequestedException()
-        if name == 'RECOGOUT':
-            # 認識結果取得開始につき結果リストを初期化
-            self._result = []
-        elif name == 'SHYPO':
-            # 文仮説取得開始につき文情報を初期化
-            self._sentence = {'SCORE': float(attrs.getValue('SCORE')),
-                                # 'GRAM':  attrs.getValue('GRAM'),
-                                'WORDS': []}
-        elif name == 'WHYPO':
-            # 単語仮説取得につき文情報に単語を追加
-            self._sentence['WORDS'].append(attrs.getValue('WORD'))
-        elif name == 'STARTRECOG':
-            self.setStartTime()
-        elif name == 'ENDRECOG':
-            self.setStopTime()
-
-        elif name == 'SYSINFO':
-            logger.info(attrs.get('PROCESS'))
-        else:
-            # その他は無視
-            pass
-
-    def endElement(self, name):
-        if name == 'RECOGOUT':
-            # 認識結果取得終了につき結果リストを表示
-            prettyPrintJuliusRecogResult(self._result)
-            # 認識結果を Dialog_manager に投げる
-            self.dialog_manager.set_user_speak(self._result[0]['WORDS'])
-        elif name == 'SHYPO':
-            # 文仮説取得終了につき，結果を結果リストに追加
-            self._result.append(self._sentence)
-
-    def characters(self, content):
-        # 文字列は無視
-        pass
-
-
-def prettyPrintJuliusRecogResult(result):
-    u"""JuliusResultHandlerがまとめた認識結果を見やすく表示する"""
-    logger.info('-----')
-    for i, r in zip(range(len(result)), result):
-        print(u'認識結果%d（スコア %f)' % (i + 1, r['SCORE']))
-        print(u'  ' + ' '.join(r['WORDS']))
-    logger.info('-----')
-
-
-class ParserThread(Thread):
-    u"""パーサを別スレッドで実行するためのスレッド"""
-
-    def __init__(self, stream):
-        u"""コンストラクタ"""
-        super(ParserThread, self).__init__()
-        # 実行ステータス．Falseの場合は次回のパージング時に例外で停止する
-        self._running = True
-        # パーサ実体
-        self._parser = xml.sax.make_parser()
-        # ハンドラ設定
-        self._parser.setContentHandler(JuliusResultHandler(self))
-        # ストリーム
-        self._stream = stream
-
-    def run(self):
-        try:
-            # パージング開始
-            self._parser.parse(StreamWrapperForJulius(self._stream))
-        except TerminateRequestedException:
-            # 終了要求後に正常に終了したことを表すので単にreturnする
-            return
-
-    def terminate(self):
-        u"""スレッドを停止する"""
-        # 実行停止要求
-        self._running = False
-        # コマンドを投げて，その返答を受取ることによって
-        # わざとパージングをすすめるトリック
-        self._stream.write('STATUS\n')
-        self._stream.flush()
-
-
 # ユーザが何を言ったかを受け取って処理する部分．
 class Dialog_manager():
     def __init__(self):
@@ -484,27 +337,23 @@ class Dialog_manager():
         print(string)
         self.user_speak = "".join(string)
         if 'こん' in self.user_speak:
-            speak_jtalk('こんにちは')
+            say_command('こんにちは')
 
 
 # 名前空間を汚すので基本は main 関数を作ってあげる
 def main():
-    threading.Thread(target=speak_read).start()
-
-    # Juliusへの接続
-    host = 'localhost'
-    port = 10500
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.connect((host, port))
-    stream = sock.makefile()
-    parserThread = ParserThread(stream)
-    parserThread.start()
-
+    # threading.Thread(target=speak_read).start()
     atexit.register(at_exit)
-    TestApp().run()
+    testApp.run()
 
 
 if __name__ == '__main__':
+    server_is_running = True
+    user_state = None
+    dialog_flag = True
+    openjtalk = Openjtalk_straight()
+    picture_list = []
+    testApp = TestApp()
     main()
 
 # vim:set foldmethod=marker:
